a0e77c76f491071f09ada7794a0751eebdcbe860
“Liam <okeefe.liam@yahoo.com>
2022-10-03 10:52:50 -0500

    commit tree 3d93a95a3dbed07d7efe8dd6fb804240bee58677

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Your code here
+function createEmployeeRecord(employee) {
+
+    return {
+        firstName: employee[0],
+        familyName: employee[1],
+        title: employee[2],
+        payPerHour: employee[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    }
+}
+
+function createEmployeeRecords(employees) {
+    return employees.map(employee => createEmployeeRecord(employee))
+}
+
+function createTimeInEvent(event) {
+    let [date, hour] = event.split(" ")
+    console.log("date", date)
+    let eventObj = {
+        type: "TimeIn",
+        hour,
+        date
+    }
+    
+    this.timeInEvents.push(eventObj)
+}
